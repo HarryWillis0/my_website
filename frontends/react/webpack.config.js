be9da186b47cd1c17d19277ca4a9c0907f2e4ab9
Harry Willis <harry.willis0@gmail.com>
2024-03-30 18:40:36 -0600

    Setup node.js server
    - setup react webpack to output builds to server folders
    - adding backends/servers/node folder with
    - setup index.ts node server that uses express static middleware to serve files from /dist folder and serve index.html by default

--- frontends/react/webpack.config.js
+++ frontends/react/webpack.config.js
@@ -1,35 +1,55 @@
-const prod = process.env.NODE_ENV === "production";
-
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode: prod ? "production" : "development",
-    entry: "./src/index.tsx",
-    output: {
-        path: __dirname + "/dist/",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                resolve: {
-                    extensions: [".ts", ".tsx", ".js", ".json"],
+const path = require("path");
+const prod = process.env.NODE_ENV === "production";
+
+const serverDists = ["node"];
+
+const webpackConfigs = serverDists.map((dist) => {
+    return {
+        mode: prod ? "production" : "development",
+        entry: "./src/index.tsx",
+        output: {
+            filename: "bundle.min.js",
+            path: path.resolve(
+                `${__dirname}/../../backends/servers/${dist}/dist`
+            ),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/,
+                    resolve: {
+                        extensions: [".ts", ".tsx", ".js", ".json"],
+                    },
+                    use: "ts-loader",
                 },
-                use: "ts-loader",
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+            ],
+        },
+        devtool: prod ? undefined : "source-map",
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+                minify: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeRedundantAttributes: true,
+                    removeScriptTypeAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    useShortDoctype: true,
+                },
+            }),
+            new MiniCssExtractPlugin({
+                filename: "styles.min.css",
+            }),
         ],
-    },
-    devtool: prod ? undefined : "source-map",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-        }),
-        new MiniCssExtractPlugin(),
-    ],
-};
+    };
+});
+
+module.exports = webpackConfigs;
