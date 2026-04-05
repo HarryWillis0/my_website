22bd53e3689d39afe5deca6892a4725b16a47577
harry <>
2026-04-05 16:27:49 -0600

    22 - Add nav to error page
    - moved layout to shell that renders children
    - updated layou to use shell and render Outlet as child
    - updated errorPage to use shell

--- frontends/react/src/pages/errorPage.tsx
+++ frontends/react/src/pages/errorPage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import { Shell } from "../components/shell";
+
 export const ErrorPage: React.FC = () => {
 	return (
-		<div className="flex flex-col items-center mt">
-			<p className="text-3xl mb-5">Oops!</p>
-			<p>Something went wrong</p>
-		</div>
+		<Shell>
+			<div className="border-t border-gray-200 pt-6">
+				<h1 className="font-serif text-3xl font-semibold text-gray-900 mb-4 leading-snug">
+					Something has gone wrong
+				</h1>
+				<p className="text-sm text-gray-500 leading-relaxed">Please try again.</p>
+			</div>
+		</Shell>
 	);
 };
